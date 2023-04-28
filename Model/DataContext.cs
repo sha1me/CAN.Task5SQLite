31742a5a10c7af4aa8e9c9cbde12b4a416d3583b
SHPAK <gaylord@gmail.com>
2023-04-28 10:43:11 +0300

    Добавьте файлы проекта.

---
+++ Model/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAN.Task5SQLite.Model
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = CAN.Task5SQLite.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
---
+++ Model/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CAN.Task5SQLite.Model
+{
+    public class Role
+    {
+        [Key]
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+    }
+}
